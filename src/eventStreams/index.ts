5df59714b86c662a8dc02aeb35d6ce28c9332985
tomsimpkins <tom.simpkins@concentra.co.uk>
2022-09-08 15:46:09 +0100

    merge in frame updates

--- src/eventStreams/index.ts
+++ src/eventStreams/index.ts
@@ -1,13 +1,17 @@
-import { merge} from "rxjs";
+import { merge } from "rxjs";
 
-import {createWheelObservable, createDragClickObservable } from "./mouseEventStream"
-import {createKeyboardObservable } from "./keyboardEventStream"
-
+import {
+  createWheelObservable,
+  createDragClickObservable,
+} from "./mouseEventStream";
+import { createKeyboardObservable } from "./keyboardEventStream";
+import { animationFrameObservable } from "./animationFrameEventStream";
 
 export const createInputEventObservable = (element: HTMLElement | Document) => {
-    return merge(
-      createDragClickObservable(element),
-      createWheelObservable(element),
-      createKeyboardObservable(element)
-    );
-  };
+  return merge(
+    createDragClickObservable(element),
+    createWheelObservable(element),
+    createKeyboardObservable(element),
+    animationFrameObservable
+  );
+};
