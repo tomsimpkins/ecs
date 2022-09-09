88d3281717b7fc2aa16498a6c93b243decc467d9
tomsimpkins <tom.simpkins@concentra.co.uk>
2022-09-09 09:50:00 +0100

    icicle, column, row

--- src/graphics/shapes.ts
+++ src/graphics/shapes.ts
@@ -26,6 +26,82 @@ export const compileColumn = (o: ColumnShape): HighLevelShape[] => {
   return [];
 };
 
+export const row = (): HighLevelShape[] => {
+  const res: HighLevelShape[] = [];
+  for (let i = 0; i < 10; i++) {
+    res.push({
+      type: "rect",
+      x: 200 + i * 22,
+      y: 200,
+      w: 20,
+      h: 20,
+      nodeReference: i,
+    });
+  }
+
+  return res;
+};
+
+export const column = (): HighLevelShape[] => {
+  const res: HighLevelShape[] = [];
+  for (let i = 0; i < 10; i++) {
+    res.push({
+      type: "rect",
+      x: 200,
+      y: 200 + i * 22,
+      w: 20,
+      h: 20,
+      nodeReference: i,
+    });
+  }
+
+  return res;
+};
+
+export const icicle = (): HighLevelShape[] => {
+  const shuffle = <T>(els: T[]): T[] => {
+    const res: T[] = [];
+
+    for (let i = 0; i < els.length; i++) {
+      const j = (Math.random() * (els.length - i)) | 0;
+      res[i] = els[j];
+
+      els[j] = els[els.length - i - 1];
+    }
+
+    return res;
+  };
+
+  const result: HighLevelShape[] = [];
+  const maxChildren = 10;
+  const queue = [{ depth: 0, y: 200, x: 200, w: 800 }]; // queue so the traversal is breadth first
+  const h = 20;
+  const [yGap, xGap] = [4, 2];
+
+  while (queue.length && result.length < 100) {
+    const { depth, x, y, w } = queue.shift()!;
+
+    result.push({ type: "rect", x: x, y: y, w: w, h: h });
+
+    const childrenCount = (Math.random() * maxChildren) | 0;
+    const widthPerChild = (w - (childrenCount - 1) * xGap) / childrenCount;
+
+    const nextElements = [];
+    for (let i = 0; i < childrenCount; i++) {
+      nextElements.push({
+        depth: depth + 1,
+        y: y + h + yGap,
+        x: x + i * (widthPerChild + xGap),
+        w: Math.max(widthPerChild, 0),
+      });
+    }
+
+    queue.push(...shuffle(nextElements));
+  }
+
+  return result;
+};
+
 export const compileShapes = (shapes: HighLevelShape[]): LowLevelShape[] => {
   const result: LowLevelShape[] = [];
   for (const shape of shapes) {
